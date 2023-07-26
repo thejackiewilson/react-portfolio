05ca2ec090c3eab6f6cadce229e2ac1015cc9cf7
Jackie <104796329+thejackiewilson@users.noreply.github.com>
2023-07-26 17:17:38 -0400

    Update Info.js

--- src/info/Info.js
+++ src/info/Info.js
@@ -6,7 +6,7 @@ import StockiePYY from "../img/StockiePYY.png"
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
 
-export let colors = (to right, rgb(130, 96, 48), rgb(91, 115, 89));
+export let colors = ["rgb(43, 69, 41)", "rgb(255, 168, 0)"];
 /*
 I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
 These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
