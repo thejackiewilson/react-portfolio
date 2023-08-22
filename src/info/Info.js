65463034768206a8dd74469e94ba2f5dd8a4b8ef
Jackie <104796329+thejackiewilson@users.noreply.github.com>
2023-08-22 08:59:29 -0400

    Update Info.js

--- src/info/Info.js
+++ src/info/Info.js
@@ -24,7 +24,7 @@ export const info = {
     firstName: "Jackie",
     lastName: "Wilson",
     initials: "JW", // the example uses first and last, but feel free to use three or more if you like.
-    position: "a tech-saavy MBA",
+    position: "a tech-savvy MBA",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
